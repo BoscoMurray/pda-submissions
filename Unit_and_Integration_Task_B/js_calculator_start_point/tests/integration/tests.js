a09d9daf9d8273c4817a93385f994e1f1c3f56cc
BoscoMurray <rossamurray@gmail.com>
2017-09-15 11:28:15 +0100

    integration test decimals ok

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
@@ -72,4 +72,14 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('-4')
   })
 
+  it('should manage decimal numbers', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('3.3333333333333335')
+  })
+
 });
